87ee3c3d83296f748182d6cdf71b218c096b6ed3
SrZek <zekonerap@gmail.com>
2026-08-14 18:53:59 +0200

    Enable automatic categories for new stations

--- ModConfig.cs
+++ ModConfig.cs
@@ -4,5 +4,6 @@ public sealed class ModConfig
 {
     public bool EnableDebugCommands { get; set; } = true;
     public bool PlayWarpSound { get; set; } = true;
+    public bool AutoCategorizeNewStations { get; set; } = true;
     public string DefaultCategory { get; set; } = "Other";
 }
