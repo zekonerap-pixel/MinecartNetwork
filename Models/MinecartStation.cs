0b57dfb381b5a212df738b1043c56f3b62391d01
SrZek <zekonerap@gmail.com>
2026-08-14 18:01:18 +0200

    Store physical minecart coordinates

--- Models/MinecartStation.cs
+++ Models/MinecartStation.cs
@@ -6,11 +6,18 @@ public sealed class MinecartStation
     public string Name { get; set; } = "Minecart";
     public string LocationName { get; set; } = "";
     public string Category { get; set; } = "Other";
+
     public int TileX { get; set; }
     public int TileY { get; set; }
     public int FacingDirection { get; set; } = 2;
+
+    public int? VisualTileX { get; set; }
+    public int? VisualTileY { get; set; }
+
     public bool HasTracks { get; set; } = true;
     public bool HasWallHole { get; set; }
     public bool IsEnabled { get; set; } = true;
     public long CreatedByPlayerId { get; set; }
+
+    public bool HasPhysicalMinecart => this.VisualTileX.HasValue && this.VisualTileY.HasValue;
 }
