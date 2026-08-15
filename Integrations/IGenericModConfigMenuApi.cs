3d9efbed112aaf2e6c01ebb1feb76983e8d05602
SrZek <zekonerap@gmail.com>
2026-08-15 15:13:02 +0200

    Add Generic Mod Config Menu API contract

---
+++ Integrations/IGenericModConfigMenuApi.cs
@@ -0,0 +1,24 @@
+using StardewModdingAPI;
+
+namespace MinecartNetwork.Integrations;
+
+internal interface IGenericModConfigMenuApi
+{
+    void Register(
+        IManifest mod,
+        Action reset,
+        Action save,
+        bool titleScreenOnly = false
+    );
+
+    void AddTextOption(
+        IManifest mod,
+        Func<string> getValue,
+        Action<string> setValue,
+        Func<string> name,
+        Func<string>? tooltip = null,
+        string[]? allowedValues = null,
+        Func<string, string>? formatAllowedValue = null,
+        string? fieldId = null
+    );
+}
